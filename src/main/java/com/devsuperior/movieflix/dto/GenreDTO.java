2bf674e3f3fa37a58ce3880570f00102735d559a
Mariana Santos <maririb51@gmail.com>
2023-09-15 11:39:59 +0100

    Endpoints para testar o Genre

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,23 +1,39 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Genre;
+
 public class GenreDTO {
 
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    public Long getId() {
-        return id;
-    }
+	public GenreDTO() {
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	}
 
-    public String getName() {
-        return name;
-    }
+	public GenreDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public GenreDTO(Genre entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
